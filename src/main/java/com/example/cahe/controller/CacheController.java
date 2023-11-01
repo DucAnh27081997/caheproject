a36e969a97a3b25d3fb5f8ec1c0cdc9918190f07
ducla <ducla@gmail.com>
2023-11-01 17:27:43 +0700

    update code d2t

--- src/main/java/com/example/cahe/controller/CacheController.java
+++ src/main/java/com/example/cahe/controller/CacheController.java
@@ -19,5 +19,8 @@ public class CacheController {
         return myService.getCachedData(key);
     }
 
-
+    @GetMapping(value = "/get-data-4")
+    public String[] getData4(String key) {
+        return myService.getCachedData(key);
+    }
 }
