7288189c8630b33e91e048f83ec7f2dc62528435
ducla <ducla@gmail.com>
2023-11-01 17:37:30 +0700

    add HomeController.java

--- src/main/java/com/example/cahe/controller/HomeController.java
+++ src/main/java/com/example/cahe/controller/HomeController.java
@@ -1,2 +1,7 @@
-package com.example.cahe.controller;public class HomeController {
+package com.example.cahe.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
 }
